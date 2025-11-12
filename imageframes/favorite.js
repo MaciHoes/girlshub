65e19d1454e27ee5772f6f99bbf2105274147231
MaciHoes <MacitheBest123@gmail.com>
2025-11-13 05:38:09 +0900

    Add new image 'IMG_8202.webp' to favoritesFinsiehd

--- imageframes/favorite.js
+++ imageframes/favorite.js
@@ -3,13 +3,14 @@
     "images/BestGirl2.jpeg",
     "images/BestGirl3.jpeg",
     "images/BestGirl4.jpeg",
+  
     "images/BestGirl5.jpeg",
     "images/BestGirl6.jpeg",
     "images/BestGirl7.jpg", // Note the .jpg extension here
     "images/IMG_8116.jpeg",
     "images/IMG_8118.jpeg",
     "images/BestGirl8.jpeg",
-
+    images/IMG_8202.webp,
 
  ];
  
