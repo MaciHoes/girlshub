9019d9e85c5adb1759aefd272a6ffda5f351e1c0
MaciHoes <MacitheBest123@gmail.com>
2025-12-20 05:58:01 -0800

    Fix image filename by adding missing extension

--- imageframes/Girlview.js
+++ imageframes/Girlview.js
@@ -37,7 +37,7 @@ const imageFilenames = [
    "images/IMG_8351.png",
    "images/Temp11.webp",
    "images/IMG_8380.png",
-   "images/IMG_8381",
+   "images/IMG_8381.png",
    "images/IMG_8383.png",
    "images/IMG_8384.png",
    "images/IMG_8385.png",
