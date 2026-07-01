b13ac1bf9a0c3749b2e66084e002a25da9ebae82
MaciHoes <MacitheBest123@gmail.com>
2026-06-30 17:46:08 -0700

    Update newview.js

--- imageframes/newview.js
+++ imageframes/newview.js
@@ -2,6 +2,10 @@
 const imageFilenames = [
     "NewView/NewView1.png",
     "NewView/CoolView1.png",
+    "NewView/IMG_9271.png",
+    "NewView/IMG_9273.png",
+    "NewView/IMG_9274.png",
+    "NewView/IMG_9276.png",
 
 
 ];
