21285124d5956f71d00be047e4585ec3cff425a6
MaciHoes <MacitheBest123@gmail.com>
2026-06-30 17:53:44 -0700

    Add new image files to NewView
    Added new image files to the NewView array.

--- imageframes/newview.js
+++ imageframes/newview.js
@@ -8,6 +8,10 @@ const imageFilenames = [
     "NewView/IMG_9276.png",
     "NewView/IMG_9143.png",
     "NewView/IMG_9144.png",
+    "NewView/IMG_9089.png",
+    "NewView/IMG_9090.png",
+    "NewView/IMG_9091.png",
+    "NewView/IMG_9092.png",
 
     "NewView/IMG_9140.png",
     "NewView/IMG_9141.png",
